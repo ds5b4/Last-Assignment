b792a3fc7bc5f6226698930c3b4c10d2e4f63048
Faithel <fvvnr5@mst.edu>
2013-12-02 19:59:33 -0600

    Create bus.h
    business class

---
+++ bus.h
@@ -0,0 +1,14 @@
+//Faith Van Wig
+//David Strickland
+
+#include <iostream>
+#include <cstring>
+#include "customer.h"
+
+using namespace std;
+
+class business
+{
+  private:
+    product items[];
+    
