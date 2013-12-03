5dcf4a1af87841643617e63bba792e9a537ce92a
Faithel <fvvnr5@mst.edu>
2013-12-02 20:08:06 -0600

    Create bus.cpp

---
+++ bus.cpp
@@ -0,0 +1,40 @@
+#include "bus.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int PEOPLE = 20;
+
+
+void business::addCustomer(customer c)
+{
+  m_sizeCusts++;
+  return;
+}
+
+sell_stuff()
+{
+  bool success;
+  ifstream fin;
+  ofstream fout;
+
+  fin.open("people.txt");
+  fin.getline (
+
+  for (int i = 0; i < ; i++)
+  {
+    if (c.buy_something(product & item) == success)
+    {
+      //transfer product to the customer and transfer funds
+}
+
+void customers_leave (customer customers[], int numOfCusts)
+{
+  customer street[PEOPLE];
+
+  for (int i = 0; i < numOfCusts; i++)
+  {
+
+
+#endif
