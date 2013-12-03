40445ff3a6d74e0e91864390c9fc2f675687801c
Faithel <fvvnr5@mst.edu>
2013-12-02 22:37:30 -0600

    Update bus.cpp

--- bus.cpp
+++ bus.cpp
@@ -16,35 +16,52 @@ const int PEOPLE = 20;
 
 void business::addCustomer(customer c)
 {
+  
   m_sizeCusts++;
   return;
 }
 
-sell_stuff()
+void sell_stuff()
 {
   bool success;
   ifstream fin;
   ofstream fout;
+  
+  m_custs[i].modMoney(float price)
+  
 
-
-  for (int i = 0; i < ; i++)
+  for (int i = 0; i < m_sizeCusts; i++)
   {
-    if (c.buy_something(product & item) == success)
+    if (c.buy_something(Product & item) == success)
     {
-      //transfer product to the customer and transfer funds
       
+      //transfer product to the customer (in array) and transfer funds
       
-      fin.open("")
     }
+  }
+  return;
 }
 
-void customers_leave (customer customers[], int numOfCusts)
-{
-  customer street[PEOPLE];
 
+
+void customers_leave (Customer street[], int numOfCusts)
+{
   for (int i = 0; i < numOfCusts; i++)
   {
+    street[i].setMoney(m_custs[i].m_money);
+    street[i].setItems(m_custs[i].m_things[m_numThings]);
+  }
+  m_custs[] = {};
+  numOfCusts = 0;
+  return;
+}  
+  
+Business::Business(const string name):Product storeItems[],
+{
+  m_name = name;
+  for(int i = 0; i < m_items; i++)
+  {
     
-Business::Business(const string name):
-
+  }
+}
 #endif
