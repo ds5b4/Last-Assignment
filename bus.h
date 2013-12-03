c7e9e56d9bd530f8b122876107a2696b55fcfdf2
Faithel <fvvnr5@mst.edu>
2013-12-02 21:26:16 -0600

    Update bus.h

--- bus.h
+++ bus.h
@@ -6,22 +6,34 @@
 
 #include "cust.h"
 #include <iostream>
-#include <string>
+#include <cstring>
 
 using namespace std;
+
+const int NUM_OF_ITEMS = 8
+const int NUM_OF_CUSTS = 12
 
 class Business
 {
   private:
-    product m_items[];
+    product m_items[NUM_OF_ITEMS];
     string m_name;
-    string m_custs[];
+    string m_custs[NUM_OF_CUSTS];
     int m_sizeCusts;
   public:
     Business(const string name):string m_name = name,
+    //Desc: adds customer into business
+    //Pre: none
+    //Post: adds cusomtomer into busness
     addCustomer(customer c){m_sizeCusts++; return;}
+    //Desc:
+    //Pre:
+    //Post:
     sell_stuff()
-    customers_leave(customer customers[], int numOfCusts);
+    //Desc:
+    //Pre:
+    //Post:
+    customers_leave(customer custs[] int numOfCusts);
 };
 
 #endif
